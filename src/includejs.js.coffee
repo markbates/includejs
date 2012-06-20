9bd9eaabc25195f8afe90b32d0f5beed5352837c
Mark Bates <mark@markbates.com>
2012-06-20 15:18:45 -0400

    Included some documentation in the source code.

--- src/includejs.js.coffee
+++ src/includejs.js.coffee
@@ -4,15 +4,33 @@ Includejs
 https://github.com/markbates/includejs
 ###
 @include = (klass, modules...) =>
+  ###
+  Since we can't assume there are any other
+  libraries available to us, let's create our
+  own simple binding function to bind the 
+  module's functions to the original object.
+  ###
   __binder = (fn, me) ->
     return ->
       return fn.apply(me, arguments)
   
+  ###
+  Loop through all of the modules that are passed in.
+  ###
   for mod in modules
-
+    ###
+    If the module has a 'included' function
+    then let's call it and pass it the object
+    that's including the module.
+    ###
     if mod.included?
       mod.included(klass)
     
     for key, value of mod
+      ###
+      As long as the 'key' is not the 'included' or
+      'include' function, then let's include it in the
+      original object and bind it together.
+      ###
       unless key in ["included", "include"]
         klass[key] = __binder(value, klass)
--- vendor/assets/javascripts/includejs.js
+++ vendor/assets/javascripts/includejs.js
@@ -13,14 +13,31 @@ https://github.com/markbates/includejs
   this.include = function() {
     var key, klass, mod, modules, value, __binder, _i, _len, _results;
     klass = arguments[0], modules = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+    /*
+      Since we can't assume there are any other
+      libraries available to us, let's create our
+      own simple binding function to bind the 
+      module's functions to the original object.
+    */
+
     __binder = function(fn, me) {
       return function() {
         return fn.apply(me, arguments);
       };
     };
+    /*
+      Loop through all of the modules that are passed in.
+    */
+
     _results = [];
     for (_i = 0, _len = modules.length; _i < _len; _i++) {
       mod = modules[_i];
+      /*
+          If the module has a 'included' function
+          then let's call it and pass it the object
+          that's including the module.
+      */
+
       if (mod.included != null) {
         mod.included(klass);
       }
@@ -29,6 +46,12 @@ https://github.com/markbates/includejs
         _results1 = [];
         for (key in mod) {
           value = mod[key];
+          /*
+                As long as the 'key' is not the 'included' or
+                'include' function, then let's include it in the
+                original object and bind it together.
+          */
+
           if (key !== "included" && key !== "include") {
             _results1.push(klass[key] = __binder(value, klass));
           } else {
